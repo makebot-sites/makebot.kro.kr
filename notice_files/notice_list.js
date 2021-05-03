a572426a8006811f0cc987935bd1600fe9f77214
WONDAEHAN <74443021+wondaehan@users.noreply.github.com>
2021-05-03 23:37:34 +0900

    Update notice_list.js

--- notice_files/notice_list.js
+++ notice_files/notice_list.js
@@ -1,4 +1,4 @@
-var titles = [];
-var dates = [];
-var ids = [];
-var writers = [];
+var titles = ['test1', 'test2'];
+var dates = ['2021.5.3', '2021.5.3'];
+var ids = ['100', '101'];
+var writers = ['u', 'u'];
