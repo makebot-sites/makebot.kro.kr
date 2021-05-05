8e7c4803c8537e3b590d251de5851501b5f5dd01
WONDAEHAN <74443021+wondaehan@users.noreply.github.com>
2021-05-05 23:27:18 +0900

    Update notice_list.js

--- notice_list.js
+++ notice_list.js
@@ -1,4 +1,4 @@
-var titles = ['test'];
-var ids = ['100'];
-var dates = ['2020.5.5'];
-var writers = ['u'];
+var titles = ['The domain of the makebot site will be transferred', 'makebot project copyright notice'];
+var ids = ['100', '101'];
+var dates = ['4.26.2021', '5.5.2021'];
+var writers = ['makebot', 'makebot'];
