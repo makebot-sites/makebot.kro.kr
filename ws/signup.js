364ca4418cb194fba949a3a1ed2d65dfdc5686a2
WONDAEHAN <74443021+wondaehan@users.noreply.github.com>
2021-10-12 23:08:33 +0900

    Update signup.js

--- ws/signup.js
+++ ws/signup.js
@@ -1,6 +1,3 @@
-import { } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-app.js";
-import { } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js";
-
 const firebaseConfig = {
   apiKey: "AIzaSyAA8dvIvVDvqfop_jL08O9oY6m89y45Wyw",
   authDomain: "makebot-accounts.firebaseapp.com",
