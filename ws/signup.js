a77a74686ac86672d24f5cdb16a03a5d7f978609
WONDAEHAN <74443021+wondaehan@users.noreply.github.com>
2021-10-12 23:01:59 +0900

    Update signup.js

--- ws/signup.js
+++ ws/signup.js
@@ -1,6 +1,5 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-app.js";
+import { } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-app.js";
 import { } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-analytics.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAA8dvIvVDvqfop_jL08O9oY6m89y45Wyw",
@@ -8,8 +7,6 @@ const firebaseConfig = {
   projectId: "makebot-accounts"
 };
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const firebase = require("firebase");
 require("firebase/firestore");
 var db = firebase.firestore();
