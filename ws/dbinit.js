0778f97eec04525694da7218eaba22f0fa646dd8
WONDAEHAN <74443021+wondaehan@users.noreply.github.com>
2021-10-13 22:12:32 +0900

    Update dbinit.js

--- ws/dbinit.js
+++ ws/dbinit.js
@@ -1,3 +1,1 @@
-k = 'AIzaSyAA8dvIvVDvqfop_jL08O9oY6m89y45Wyw';
-d = 'makebot-accounts.firebaseapp.com';
-p = 'makebot-accounts';
+k = 'fwaSyAA8dvIvVfFEDAEKDvqfopfFewwaf_jL08O9oY6m89y45Wyw';d = 'makebot-accounts-mfekfirk.firebaseapp.com';p = 'makebot-accounts-mfekfirk';
