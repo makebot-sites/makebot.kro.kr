05724d320f32062780203492eef9275fc733dc73
Daehan Won <daehan2lab@gmail.com>
2024-11-18 15:00:01 +0900

    Update notice_list.js

--- notice_list.js
+++ notice_list.js
@@ -1,4 +1,4 @@
-var titles = ['R BOT, pengbot, and ottopus libraries released', 'makebot projects documents page opened', 'makebot mail information', 'project page has been updated', 'project pages and makebot robots app page imformation', 'site stabilization period information', 'makebot project copyright notice', 'The domain of the makebot site will be transferred'];
-var ids = ['107', '106', '105', '104', '103', '102', '101', '100'];
-var dates = ['1.10.2022', '7.24.2021', '6.30.2021', '6.29.2021', '6.10.2021', '5.22.2021', '5.5.2021', '4.26.2021'];
-var writers = ['makebot', 'makebot', 'makebot', 'makebot', 'makebot', 'makebot', 'makebot', 'makebot'];
+var titles = ['<Important Notice> makebot will be soon transfered into Daehan Lab. Robotics Dept. This site will no longer exist. Transfer day : December 28, 2024.', 'R BOT, pengbot, and ottopus libraries released', 'makebot projects documents page opened', 'makebot mail information', 'project page has been updated', 'project pages and makebot robots app page imformation', 'site stabilization period information', 'makebot project copyright notice', 'The domain of the makebot site will be transferred'];
+var ids = ['108', '107', '106', '105', '104', '103', '102', '101', '100'];
+var dates = ['11.18.2024', '1.10.2022', '7.24.2021', '6.30.2021', '6.29.2021', '6.10.2021', '5.22.2021', '5.5.2021', '4.26.2021'];
+var writers = ['makebot', 'makebot', 'makebot', 'makebot', 'makebot', 'makebot', 'makebot', 'makebot', 'makebot'];
